6c3f4e845a0cef22a0ed9e0f8261bea34514d2e9
SpillenX <spillenx@gmail.com>
2021-02-08 14:37:21 +0100

    Implemented hook effects

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,22 @@
-import FetchCurrencies from '../api/FetchCurrencies'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Main = () => {
+  //const currencies = {}
+  const URL = 'https://v6.exchangerate-api.com/v6/86ac07a01d57484529525c6c/latest/SEK'
+  const [currency, setCurrency] = useState(null)
 
-  let currencies = {}
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await axios.get(URL)
+      setCurrency(result.data)
+    }
+    fetchData()
+    console.log(currency)
+  }, [currency===null])
 
-  const fetchCurrencies = async() => {
-    const data = await FetchCurrencies()
-
-    currencies = data
-    console.log(currencies.conversion_rates)
+  function refreshCurrencies() {
+    setCurrency(null)
   }
 
   return ( 
@@ -16,19 +24,21 @@ const Main = () => {
       <div className="main-container">
         <div className="currenct-exchange-container">
           <div className="currency-select-container">
-
+            <div className="dropdown">
+              <button className="currency-button" id="from-currency">From</button>
+              <button className="currency-button" id="to-currency">To</button>
+              <div className="dropdown-content"></div>
+            </div>
           </div>
           <div className="currency-input-container">
             <input className="currency-select" id="from-currency"></input>
             <input className="currency-select" id="to-currency"></input>
           </div>
-
           <div className="currency-output-container">
             <p>Received amount: <span className="currency-received"></span></p>
           </div>
-
         </div>
-        <button onClick={fetchCurrencies}>Update</button>
+          <button className="update-currencies" onClick={ refreshCurrencies }>Update currencies</button>
         </div>
     </div>
    )
