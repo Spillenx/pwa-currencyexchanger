99173ac0e15d15191ec331eee52e6b63470bbf0e
SpillenX <spillenx@gmail.com>
2021-02-13 13:49:38 +0100

    updated css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Main />
     </div>
   )
