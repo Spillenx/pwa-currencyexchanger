f11c17c6b36f092563f3e9edc1256b7e1bc2bd5a
SpillenX <spillenx@gmail.com>
2021-02-17 23:42:05 +0100

    updated app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import Main from "./components/Main"
+import { useState, useEffect } from 'react'
+import { fetchCurrencyAPI } from './api/fetchCurrencyAPI'
+import Header from "./components/Header"
+import ExchangeMain from "./components/ExchangeMain"
+import Footer from './components/Footer'
+import './App.css'
 
 const App = () => {
+
+  const [currency, setCurrency] = useState(null)
+
+  useEffect(() => {
+    if(currency === null) {
+      try{
+        console.log('fetch')
+        const fetchData = async() => {
+          const result = await fetchCurrencyAPI()
+          setCurrency(result)
+        }
+        fetchData()
+      } catch (e) {
+        console.log('Error: ', e)
+      }
+
+    }
+  }, [currency])
+
   return (
     <div className="app">
-      <Main />
+      <div className="main-container">
+        <Header />
+        <ExchangeMain currency={currency}/>
+        <Footer currency={currency}/>
+      </div>
     </div>
   )
 }
